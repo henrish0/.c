ce99df6eb059ef8733b1e8d93333fcf5c0bbb7c5
Henrique Almeida <hcarvalhoalmeida7@gmail.com>
2022-05-12 09:19:01 -0300

    Update 1a lista de algoritmos e programas em C.c

--- 1a lista de algoritmos e programas em C.c
+++ 1a lista de algoritmos e programas em C.c
@@ -8,7 +8,7 @@
 main()
 {
     int exc;
-    printf(" 1a lista de algoritmos e programas em C\n");
+    printf("\n 1a lista de algoritmos e programas em C\n");
 exec:
     printf(" Digite um exercio [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 , 15, 16, 17, 18, 19, 20 , 21, 22, 23, 24, 25, 26, 27]: ");
     scanf("%i", &exc);
