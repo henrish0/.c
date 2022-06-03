6506cec32083208688a55ea7c2e6170b034a6b65
Henrique Almeida <hcarvalhoalmeida7@gmail.com>
2022-06-03 13:58:41 -0300

    Update ler lista e armazenar em matriz.c

--- ler lista e armazenar em matriz.c
+++ ler lista e armazenar em matriz.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 main(){
 FILE*pArquivo;
-pArquivo=fopen("lista.txt","r");
-int ag,ge,im,age,weight,matrix[2][4][4];float height,imc;char name[100],gender;
-for(int i=0;i<2;i++)for(int j=0;j<4;j++)for(int k=0;k<4;k++)matrix[i][j][k]=0;
+pArquivo = fopen("lista.txt","r");
+int ag,ge,im,age,weight,matrix[2][4][4]; float height,imc; char name[100],gender;
+for (int i=0;i<2;i++) for (int j=0;j<4;j++) for (int k=0;k<4;k++) matrix[i][j][k]=0;
 while(fscanf(pArquivo,"%s %i %c %f %i",&name,&age,&gender,&height,&weight)!= EOF){
-    imc=weight/(height*height);
+    imc = weight/(height*height);
     // printf("\n %s %i %c %.2f %i  imc= %.2f",name,age,gender,height,weight,imc);
-    if(age<18)ag=0;
-    else if(age==18)ag=1;
-    else if(age==19)ag=2;
+    if (age<18) ag=0;
+    else if (age==18) ag=1;
+    else if (age==19) ag=2;
     else ag=3;
-    if(gender=='F')ge=0;
+    if (gender=='F') ge=0;
     else ge=1;
-    if(imc<18.5)im=0;
-    else if(imc>=18.5&&imc<24)im=1;
-    else if(imc>=24&&imc<29)im=2;
-    else if(imc>=29)im=3;
+    if (imc<18.5) im=0;
+    else if (imc>=18.5 && imc<24) im=1;
+    else if (imc>=24 && imc<29) im=2;
+    else if (imc>=29) im=3;
     matrix[ge][ag][im]++;}
 //printf("\n\n\n Minha matriz\n\n");
-//for(int i=0;i<2;i++)for(int j=0;j<4;j++)for(int k=0;k<4;k++)printf("\n (%i,%i,%i)= %i",i,j,k,matrix[i][j][k]);
+//for (int i=0;i<2;i++) for (int j=0;j<4;j++) for (int k=0;k<4;k++) printf("\n (%i,%i,%i)= %i",i,j,k,matrix[i][j][k]);
 fclose(pArquivo);
 }
